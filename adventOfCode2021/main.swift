136dbf328eeea6c962ac7e2a69159cf298300f42
Felix Larsen <16463534+i4guar@users.noreply.github.com>
2021-11-29 12:01:28 +0100

    Add resource file setup

--- adventOfCode2021/main.swift
+++ adventOfCode2021/main.swift
@@ -9,3 +9,20 @@ import Foundation
 
 print("Advent of Code 2021!")
 
+
+let filename = "dec00"
+
+// resource file setup https://stackoverflow.com/a/57093149/9142902
+let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+let bundleURL = URL(fileURLWithPath: "codingResources.bundle", relativeTo: currentDirectoryURL)
+let bundle = Bundle(url: bundleURL)
+let txtFileURL = bundle!.url(forResource: filename, withExtension: "txt")!
+
+do {
+    // Get the contents
+    let contents = try String(contentsOfFile: txtFileURL.path, encoding: .utf8)
+    print(contents)
+}
+catch let error as NSError {
+    print("Ooops! Something went wrong: \(error)")
+}
